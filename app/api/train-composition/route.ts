e5bd356c6354c82cfdd94faaef2c01ad46a86a05
Majersingh <allfactz07@gmail.com>
2026-02-26 16:42:41 +0530

    Dix train composition api

--- app/api/train-composition/route.ts
+++ app/api/train-composition/route.ts
@@ -5,19 +5,25 @@ export async function POST(req: NextRequest) {
   const { trainNo, jDate, boardingStation } = body;
 
   try {
-    const res = await fetch(
-      "https://www.irctc.co.in/online-charts/api/trainComposition",
-      {
-        method: "POST",
-        headers: {
-          accept: "application/json",
-          "content-type": "application/json",
-          referer: "https://www.irctc.co.in/online-charts/",
-        },
-        body: JSON.stringify({ trainNo, jDate, boardingStation }),
-        cache: "no-store",
-      }
-    );
+    const res = await fetch("https://www.irctc.co.in/online-charts/api/trainComposition", {
+      "headers": {
+        "accept": "application/json",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/json",
+        "priority": "u=1, i",
+        "sec-ch-ua": "\"Not:A-Brand\";v=\"99\", \"Google Chrome\";v=\"145\", \"Chromium\";v=\"145\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Linux\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin"
+      },
+      "referrer": "https://www.irctc.co.in/online-charts/",
+      "body": JSON.stringify({ trainNo, jDate, boardingStation }),
+      "method": "POST",
+      "mode": "cors",
+      "credentials": "include"
+    });
 
     const data = await res.json();
     return NextResponse.json(data);
